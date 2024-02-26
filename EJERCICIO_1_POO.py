e45550474452887ecedfa1360d33ec911cfb17d4
namontoya <161357317+namontoya@users.noreply.github.com>
2024-02-26 12:42:37 -0500

    Add files via upload

---
+++ EJERCICIO_1_POO.py
@@ -0,0 +1,12 @@
+#Ejercicio Resuelto No 4
+
+EDJUAN = int(9)
+EDALBER = int((2/3)*EDJUAN)
+EDANA = int((4/3)*EDJUAN)
+EDMAMA = int(EDALBER+EDJUAN+EDANA)
+
+print("LAS EDADES SON:")
+print("ALBERTO:",EDALBER)
+print("JUAN:",EDJUAN)
+print("ANA:",EDANA)
+print("MAMA:",EDMAMA)
